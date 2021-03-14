2abab4616e85a932d8a9915798c8e2a1d93162bf
Icaro Nunes <icarolimanunes@gmail.com>
2021-03-14 14:02:51 -0300

    Versao funcional com o codigo comentado

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -1,76 +1,80 @@
-/*
- Input Pullup Serial
- 
- This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a 
- digital input on pin 2 and prints the results to the serial monitor.
- 
- The circuit: 
- * Momentary switch attached from pin 2 to ground 
- * Built-in LED on pin 13
- 
- Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal 
- 20K-ohm resistor is pulled to 5V. This configuration causes the input to 
- read HIGH when the switch is open, and LOW when it is closed. 
- 
- created 14 March 2012
- by Scott Fitzgerald
- 
- http://www.arduino.cc/en/Tutorial/InputPullupSerial
- 
- This example code is in the public domain
- 
- */
- 
 
-unsigned char valor = 127;
+//valor do duty cycle
+unsigned int valor = 127;
 
-//int teste = 0;
+//unsigned char barra = 0;
 
-int ciclos = 0;
+//quantidade de loops
+//int ciclos = 0;
+
 
 int lastRead = 0;
 
+
 void setup(){
   //start serial connection
+  //Para debug
   Serial.begin(9600);
   
-  DDRC = 0;
+  //Data Direction Register for port C
+
+  //Declarando pinos da porta C como entrada
+  DDRC = 0; //B 0000 0000
+
+  //Declarando pinos da porta B como saida
   DDRB = B11111111;
 
+  //Declarando pinos da porta D como saida
   DDRD = B11111111;
 }
 
+
 void loop(){
-  if(ciclos == 10){
-    ciclos = 0;
-    char leitura = PINC;
+//  if(ciclos == 10){
     
+//    ciclos = 0;
+
+    unsigned char leitura = PINC;
     
-    if(leitura & lastRead){
+    //Checa se o botao estava pressionado no ultimo ciclo
+    
+    if(!(leitura == lastRead)){
+      
+      //Checa ce o botao em C0 esta pressionado
       if (leitura & B00000001) {
+        
         if(valor <255){
           valor++;
         }
-        
+
+        //Leva o valor atualizado a porta D, que e a porta dos leds        
         PORTD = valor;
+
       } 
       
+      //Checa ce o botao em C1 esta pressionado
       if(leitura & B00000010) {
         
         if(valor>0){
           valor--;
         }
         
+        //Leva o valor atualizado a porta D, que e a porta dos leds
         PORTD = valor;
-        
-        analogWrite(9, valor);
+       
       }
+      
+      //Seta o pwm
+      analogWrite(9, valor);
     }
-  //  Serial.println(teste);
+    
+    //debug
     Serial.println(valor, BIN);
     
+    //Entrega esta leitura a variavel lastRead
     lastRead = leitura;
-  }
 
-    ciclos++;
+//  }
+
+//    ciclos++;
 }
