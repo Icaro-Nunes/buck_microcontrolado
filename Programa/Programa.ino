2893db6ea3fa3906c2de8fa302b9c0c9a4fc3c7e
Icaro Nunes <icarolimanunes@gmail.com>
2021-03-28 19:45:40 -0300

    Versao entregue na primeira unidade

--- Programa/Programa.ino
+++ Programa/Programa.ino
@@ -1,15 +1,8 @@
 
 //valor do duty cycle
-unsigned int valor = 127;
-
-//unsigned char barra = 0;
-
-//quantidade de loops
-//int ciclos = 0;
-
+int valor = 100;
 
 int lastRead = 0;
-
 
 void setup(){
   //start serial connection
@@ -20,6 +13,7 @@ void setup(){
 
   //Declarando pinos da porta C como entrada
   DDRC = 0; //B 0000 0000
+  
 
   //Declarando pinos da porta B como saida
   DDRB = B11111111;
@@ -30,51 +24,65 @@ void setup(){
 
 
 void loop(){
-//  if(ciclos == 10){
-    
-//    ciclos = 0;
 
     unsigned char leitura = PINC;
     
+    leitura = leitura ^ 0xFF;
+    
     //Checa se o botao estava pressionado no ultimo ciclo
     
-    if(!(leitura == lastRead)){
+    if(/*leitura != lastRead && leitura != 0*/1){
       
-      //Checa ce o botao em C0 esta pressionado
+      //Checa se o botao em C0 esta pressionado
       if (leitura & B00000001) {
         
         if(valor <255){
           valor++;
         }
 
-        //Leva o valor atualizado a porta D, que e a porta dos leds        
-        PORTD = valor;
-
       } 
       
-      //Checa ce o botao em C1 esta pressionado
+      //Checa se o botao em C1 esta pressionado
       if(leitura & B00000010) {
         
         if(valor>0){
           valor--;
         }
-        
-        //Leva o valor atualizado a porta D, que e a porta dos leds
-        PORTD = valor;
        
       }
+      
+      //Preparar barra de progresso
+      unsigned char barra = 0;
+      
+      int divisao = (valor + 1)/32;
+      Serial.print("Divisao: ");
+      Serial.println(divisao, DEC);
+      
+      //unsigned int divisao = 3;
+      
+      for(int i = 0; i<divisao; i++){
+        barra = barra << 1; // B00000000 -> B00000000
+        barra += 1; // B0000
+      }
+      
+      Serial.print("Barra: ");
+      Serial.println(barra, BIN);
+      
+      //Leva o valor atualizado a porta D, que e a porta dos leds
+      PORTD = ~barra;
       
       //Seta o pwm
       analogWrite(9, valor);
     }
     
     //debug
-    Serial.println(valor, BIN);
+    Serial.print("Leitura: ");
+    Serial.println(leitura, BIN);
+    
+    Serial.print("Valor: ");
+    Serial.println(valor, DEC);
     
     //Entrega esta leitura a variavel lastRead
     lastRead = leitura;
 
-//  }
-
-//    ciclos++;
 }
